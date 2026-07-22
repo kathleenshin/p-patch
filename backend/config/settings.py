8465a549ae919c1906666f8fa615cc4a98fb1ec3
Linnea Johnson <linnea.johnson1@gmail.com>
2026-07-22 11:45:48 -0700

    Configure Django to use Neon PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import dj_database_url
 
 load_dotenv()
 
@@ -88,12 +89,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # TODO: implement Neon integration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
