3cfe0f9ceb6a19c5d27361f6b0de5f5e8e972f27
Kathleen Shin <kathleen.n.shin@gmail.com>
2026-07-23 01:35:28 -0700

    Create user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_approved = models.BooleanField(default=False)
+    is_garden_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
