485acf0bd1fe0830d2d014725c7e77d61a57b231
Kathleen Shin <kathleen.n.shin@gmail.com>
2026-07-24 15:14:14 -0700

    update user model for unique and required email field

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
     is_approved = models.BooleanField(default=False)
     is_garden_admin = models.BooleanField(default=False)
 
