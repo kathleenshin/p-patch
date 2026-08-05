c6337a542538d3dbb78461e8539a9f5793945fa4
Linnea Johnson <linnea.johnson1@gmail.com>
2026-08-05 13:29:20 -0700

    Config urls added

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/weather/", include("weather.urls")),
     path("api/auth/", include("users.urls")),
-
+    path("api/", include("plots.urls")),
 ]
