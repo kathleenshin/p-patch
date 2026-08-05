8ff0f1460e1ac795cb7f3c7357a6102b36493933
Linnea Johnson <linnea.johnson1@gmail.com>
2026-08-05 13:24:58 -0700

    Created serializers.py

---
+++ backend/plots/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Plot
+
+
+class PlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plot
+        fields = "__all__"
