588aeefa805700be829cd0c020e0abc8ec70aaa3
Linnea Johnson <linnea.johnson1@gmail.com>
2026-08-09 22:51:51 -0700

    Fix test static files storage backend

--- backend/config/settings_test.py
+++ backend/config/settings_test.py
@@ -19,7 +19,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.core.files.storage.StaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
